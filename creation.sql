c36d70d28b7826e1111b198337519d02dd1f27f1
Mitchell Meier <mdmfvz@mst.edu>
2020-11-10 20:20:19 -0600

    final DDL and report data

--- creation.sql
+++ creation.sql
@@ -15,7 +15,7 @@ CREATE TABLE Authors(
    
 CREATE TABLE Prices(
     Book_ID BIGINT UNSIGNED NOT NULL,
-    Price DECIMAL(10,2) UNSIGNED,
+    Price DECIMAL(10,2) ZEROFILL NOT NULL,
     PRIMARY KEY(Book_ID, Price),
     CONSTRAINT fk_book_id
         FOREIGN KEY (Book_ID) REFERENCES Books (Book_ID)
@@ -49,7 +49,7 @@ CREATE TABLE Publications(
         ON DELETE CASCADE
         ON UPDATE RESTRICT);
            
-   
+
 CREATE TABLE Quality(
     Book_ID BIGINT UNSIGNED NOT NULL,
     Binding VARCHAR(255),
@@ -72,7 +72,7 @@ CREATE TABLE Languages(
         ON DELETE CASCADE
         ON UPDATE RESTRICT);
        
-    
+/*
 DROP TABLE Prices;
 DROP TABLE Languages;
 DROP TABLE Quality;
@@ -81,7 +81,6 @@ DROP TABLE Publishers;
 DROP TABLE Authors;
 DROP TABLE Books;
 
-SHOW PROCESSLIST;
 
 SELECT * FROM Prices;
 SELECT * FROM Languages;
@@ -89,3 +88,47 @@ SELECT * FROM Quality;
 SELECT * FROM Publishers;
 SELECT * FROM Authors;
 SELECT * FROM Books;
+
+
+
+SELECT a.Name, COUNT(Book_ID) AS Number_of_Books
+FROM Publications p, Authors a
+WHERE p.Author_ID = a.Author_ID
+GROUP BY a.Name
+ORDER BY a.Name
+
+
+SELECT a.Name, b.Title
+FROM Authors a, Books b, Publications p
+WHERE p.Book_ID = b.Book_ID
+    AND p.Author_ID = a.Author_ID
+ORDER BY a.Name
+
+
+SELECT COUNT(*)
+FROM Books
+
+
+SELECT Publisher, COUNT(Book_ID) AS Number_of_Books
+FROM Publishers
+WHERE Publisher IS NOT NULL
+GROUP BY Publisher
+ORDER BY Publisher
+
+SELECT b.Book_ID
+FROM Books b, Languages L, Prices pr, Publishers p, Quality q
+WHERE L.Book_ID = b.Book_ID
+    AND pr.Book_ID = b.Book_ID
+    AND p.Book_ID = b.Book_ID
+    AND q.Book_ID = b.Book_ID
+    AND (b.Release_Date IS NULL
+    OR b.Location IS NULL
+    OR L.Language IS NULL
+    OR pr.Price = 0.00
+    OR p.Publisher IS NULL
+    OR q.Binding IS NULL
+    OR q.Grade IS NULL)
+
+
+
+
