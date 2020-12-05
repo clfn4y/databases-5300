702edae4e3d5c06189402a9b1dbd3c3258fa4c4b
Kyle Nguyen <kknc55@mst.edu>
2020-12-04 18:34:13 -0600

    Cleaned up existing tkinter, added label, added TODOs

--- tkinter/main.py
+++ tkinter/main.py
@@ -47,45 +47,45 @@ root = Tk()
 root.geometry("1280x720")
 frame = Frame(root)
 
-#grid setup. row 0 is label, 1 is search field, 2 is checkboxes?, 3 is table return
-#column 2 to center your cell
+#grid setup. row 0 is label, 1 is search field, 2 is checkboxes, 3 is table return
+#column 1 to center your cell
 frame.grid(row=0, column=0, sticky="NESW")
 frame.grid_rowconfigure(0, weight=1)
 frame.grid_rowconfigure(1, weight=1)
 frame.grid_rowconfigure(2, weight=2)
 frame.grid_rowconfigure(3, weight=10)
+frame.grid_columnconfigure(0, weight=1)
 frame.grid_columnconfigure(1, weight=1)
 frame.grid_columnconfigure(2, weight=1)
-frame.grid_columnconfigure(3, weight=1)
 
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
 #Title Creation
 title = Label(
-    frame,
-    text="Database Querying Application",
-    fg="black",  
-    bg="white",
-    font=("Arial", 25)
+  frame,
+  text="Database Querying Application",
+  fg="black",
+  font=("Arial", 25)
 )
-#Positioning 
-title.grid(row=0, column=2)
+title.grid(row=0, column=1)
 
-#Dropdown Bar Creation
-#Too small rn, set the size how you like
+# Dropdown Bar Row
 
+# Label for DD Query Selection
+dd_label = Label(frame, text="Query by:", font=("Arial", 15))
+dd_label.grid(row=1, column=1, sticky="N")
+
+# Dropdown Bar Creation
 drop_down_var = StringVar(frame)
-#Default Value
-drop_down_var.set('Title')
-dd_choices = { 'Title', 'Author', 'Release Date', 'Binding', 
-'Grade', 'Language', 'Price'}
+drop_down_var.set('SELECT') # Set default text
+dd_choices = {'Title', 'Author', 'Release Date', 'Binding', 'Grade', 'Language', 'Price'}
 drop_down = OptionMenu(frame, drop_down_var, *dd_choices)
-#Positioning
-drop_down.grid(row=1,column=2)
+drop_down.config(font=("Arial", 15))
+drop_down.grid(row=1, column=1, sticky="S")
 
-
-
+# TODO: Render rows 2 + 3 based on dd_choices.get()
+# TODO: Render table based on input/selections from rows 2 + 3 on row 4
 
 
 
