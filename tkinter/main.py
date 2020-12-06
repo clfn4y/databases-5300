e815ab953d5895bf257a15e7ea265073d85f496b
Kyle Nguyen <kknc55@mst.edu>
2020-12-05 18:12:19 -0600

    added another frame, grid configuring it (might wanna add another row), other cleanup

--- tkinter/main.py
+++ tkinter/main.py
@@ -19,19 +19,6 @@ import sys
 
   # return connection
 
-def button_creation(label):
-  button = Button(
-    text=label,
-    width=25,
-    height=5,
-    bg="green",
-    fg="black",
-    font=("Arial", 15)
-  )
-
-  return button
-
-
 #conn = database_connection()
 #curr = conn.cursor()
 
@@ -47,7 +34,7 @@ root = Tk()
 root.geometry("1280x720")
 frame = Frame(root)
 
-#grid setup. row 0 is label, 1 is search field, 2 is checkboxes, 3 is table return
+#grid setup. row 0 is label, 1 is dropdown, 2 is conditional input field + checkboxes, 3 is table return
 #column 1 to center your cell
 frame.grid(row=0, column=0, sticky="NESW")
 frame.grid_rowconfigure(0, weight=1)
@@ -62,12 +49,7 @@ root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
 #Title Creation
-title = Label(
-  frame,
-  text="Database Querying Application",
-  fg="black",
-  font=("Arial", 25)
-)
+title = Label(frame, text="Database Querying Application", fg="black", font=("Arial", 25))
 title.grid(row=0, column=1)
 
 # Dropdown Bar Row
@@ -85,6 +67,23 @@ drop_down.config(font=("Arial", 15))
 drop_down.grid(row=1, column=1, sticky="S")
 
 # TODO: Render rows 2 + 3 based on dd_choices.get()
+
+inner_frame = Frame(frame)  # frame consisting of rows 2 - 3, columns 0 - 2
+inner_frame.grid(row = 2, rowspan = 2, column = 0, columnspan = 3, sticky = "NSEW")
+
+# inner_frame row configuration
+inner_frame.grid_rowconfigure(0, weight=4)  # row containing the conditional search field
+inner_frame.grid_rowconfigure(1, weight=1)  # rows containing the checkboxes (4x4 grid)
+inner_frame.grid_rowconfigure(2, weight=1)
+inner_frame.grid_rowconfigure(3, weight=1)
+inner_frame.grid_rowconfigure(4, weight=1)
+
+# inner_frame column configuration
+inner_frame.grid_columnconfigure(0, weight=1) # columns containing the checkboxes (4x4 grid)
+inner_frame.grid_columnconfigure(1, weight=1)
+inner_frame.grid_columnconfigure(2, weight=1)
+inner_frame.grid_columnconfigure(3, weight=1)
+
 # TODO: Render table based on input/selections from rows 2 + 3 on row 4
 
 
